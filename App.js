81c3bc26d06519134cd39e8e1c04d7358f6db94d
KULDEEP TRIPATHI <95953606+DMKuldeep@users.noreply.github.com>
2023-01-11 19:35:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import NavBar from "./component/NavBar";
+import CrmHomepage from "./component/CrmHomepage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fontsource/poppins";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+ 
+      
+        <CrmHomepage />
+   
+    </>
+  );
+}
+
+export default App;
